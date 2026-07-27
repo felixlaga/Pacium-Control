55e77da3fe943e100e83f2e9267cf988894ff390
felixlaga <felix.laga@yahoo.com>
2026-07-27 14:13:19 +0200

    feat(contracts): define queue item inspection

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -2,4 +2,5 @@ export * from "./directories.js";
 export * from "./pacium-config.js";
 export * from "./protocol.js";
 export * from "./queue-classification.js";
+export * from "./queue-item-inspection.js";
 export * from "./queue-observation.js";
